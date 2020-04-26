f0c52ce428e9fc4bd55bc604f22878df58aa8b7a
ajay <ajsuany@gmail.com>
2020-04-26 15:36:47 +0530

    Quiz set

---
+++ src/Quiz/Quiz4.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+import './Home.css'
+
+
+
+export default class Quiz4 extends Component{
+                          
+    constructor(){
+        super()
+        this.state = {
+            ans : 6,
+            score: 0
+
+        }
+
+    }
+ selectedOpt = (event) =>{
+         let {ans,score} = this.state
+         let temp =parseInt(event.target.innerText) 
+         if (temp === ans)
+         {
+              score+=1
+         }else{
+             score-=1
+         }
+         this.props.calculation(score)
+         this.setState({score})
+ } 
+              
+           continue = () => {
+
+               this.props.next()
+           }
+
+           back = () => {
+               console.log("back fun is called",this.props)
+            this.props.backs()
+        }
+
+             render(){
+                    
+                    return(<div>
+                                <p> Q4. 2+4= ? </p>
+                                <div className="option">
+                                <div onClick={this.selectedOpt} > 3 </div>  
+                                <div onClick={this.selectedOpt} > 4 </div>  
+                                <div onClick={this.selectedOpt} > 5 </div>  
+                                <div onClick={this.selectedOpt} > 6 </div>
+                                </div>
+                    <div>Scored Mark:{this.state.score}</div>
+                                 <div>
+                                     <button onClick={this.continue}>
+                                       Next Quiz
+                                     </button>
+                                     <button onClick={this.back}>
+                                         Back Quiz
+                                     </button>
+                                 </div>
+                            </div>
+                          )
+                    }
+
+}
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ToDoList from './todoList/ToDoList'
+import Home from './Quiz/Home'
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
  
- <ToDoList/> 
-  ,document.getElementById('root'));
+   <Home/>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls. Learn
